22dd07d76a699536dec360958232739630e80929
[Rethigamer] <[rethinaath@gmail.com]>
2024-05-15 23:26:47 +0530

    tried something on ui

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import streamlit as st
-st.write("Hello there")
+from streamlit.components.v1 import html
+with st.container():
+    html('''
+    <script>
+        var decoration = window.parent.document.querySelectorAll('[data-testid="stDecoration"]')[0];
+        decoration.style.right = "90px";
+        decoration.style.height = "40px";
+        decoration.innerText = "REC FOOD DELIVERY APP";
+        decoration.style.fontWeight = "bold";
+         decoration.style.display = "flex";
+        decoration.style.justifyContent = "center";
+        decoration.style.alignItems = "center";
+    </script>
+    ''')
