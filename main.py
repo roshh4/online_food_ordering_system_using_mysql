56f998a277fccd2471e842128a054455f6aa71c4
roshh4 <220701228@rajalakshmi.edu.in>
2024-05-20 03:50:49 +0530

    made the database and gave connection

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import streamlit as st
 st.write("Hello there")
 st.write("this is rosh")
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost", 
+            user="root",
+            password="R@shini444",
+            database="food_court"
+        )
+        if connection.is_connected():
+            print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+# Establish the connection
+conn = create_connection()
