e4ba4aa566c7f335d5531da7bce6bbe7d82629c6
roshh4 <roshinidharmraj@gmail.com>
2024-05-22 18:55:45 +0530

    before pul

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ import streamlit as st
 from customer_details import customer_details
 from choose_restaurant import choose_restaurant
 from streamlit_option_menu import option_menu
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Login", 'Restuarants'], 
-        icons=['key', 'houses'], menu_icon="cast", default_index=1)
-    
+
 # Function to establish database connection
-#roshini's
 def create_connection():
+    connection = None
     try:
         connection = mysql.connector.connect(
             host="sql12.freesqldatabase.com	", 
@@ -20,46 +17,30 @@ def create_connection():
         )
         if connection.is_connected():
             print("Connection to MySQL DB successful")
-        return connection
-    except Error as e:
-        st.error(f"The error '{e}' occurred")
-        return None
-
-#rethinaath's
-# def create_connection():
-#     connection = None
-#     try:
-#         connection = mysql.connector.connect(
-#             host="sql12.freesqldatabase.com	", 
-#             user="sql12707743",
-#             password="EfzR55xMVG",
-#             database="sql12707743"
-#         )
-#         if connection.is_connected():
-#             print("Connection to MySQL DB successful")
-#     except MySQLdb.Error as e:
-#         print(f"The error '{e}' occurred")
-#     return connection
+    except MySQLdb.Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
 
 conn = create_connection()
 
 # Function to insert customer details into the database
-def insert_customer(connection, customer_name, contact_number, email, address):
+def insert_customer(connection, customer_name, contact_number, email):
     try:
         cursor = connection.cursor()
-        query = "INSERT INTO customer (customer_name, contact_number, address, email) VALUES (%s, %s, %s, %s)"
-        cursor.execute(query, (customer_name, contact_number, email, address))
+        query = "INSERT INTO customer (customer_name, contact_number, address, email) VALUES (%s, %s, '', %s)"
+        cursor.execute(query, (customer_name, contact_number, email))
         connection.commit()
         st.success("Customer inserted successfully")
     except MySQLdb.Error as e:
         st.error(f"Error: '{e}'")
 
-# Function to get restaurant names and images from the database
-def get_restaurants(connection):
+
+#function to get restaurant names from the databse
+def get_restaurants():
     restaurants = []
     try:
-        if connection.is_connected():
-            cursor = connection.cursor()
+        if conn.is_connected():
+            cursor = conn.cursor()
             cursor.execute("SELECT restaurant_name, images FROM restaurants")
             rows = cursor.fetchall()
             restaurants = [(row[0], row[1]) for row in rows]
@@ -67,15 +48,14 @@ def get_restaurants(connection):
         st.error(f"Error retrieving restaurant names: {e}")
     return restaurants
 
-# Establish the connection
-conn = create_connection()
-
 if conn:
     if 'page' not in st.session_state:
         st.session_state['page'] = 'customer_details'
 
-    if st.session_state['page'] == 'customer_details' and selected=="Login":
+    if st.session_state['page'] == 'customer_details':
         customer_details(conn, insert_customer)
-    if selected=="Restuarants":
-        restaurants = get_restaurants(conn)
+    if st.session_state['page'] == 'choose_restaurant':
+        restaurants = get_restaurants()
+        with st.sidebar:
+            selected = option_menu("Main Menu", ["Login", 'Restuarants'], icons=['key', 'houses'], menu_icon="cast", default_index=1)
         choose_restaurant(restaurants)
