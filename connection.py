cd96a2b4af1fd0ab9a6157b77930d4df72cca40c
[Rethigamer] <[rethinaath@gmail.com]>
2024-05-27 22:25:45 +0530

    commit

--- connection.py
+++ connection.py
@@ -1,13 +1,30 @@
 import mysql.connector
 import MySQLdb
 
-def create_connection():
+#LOCALHOST
+'''def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
             host="localhost", 
             user="root",
             password="R@shini444",
+            database="sql12707743"
+        )
+        if connection.is_connected():
+            print("Connection to MySQL DB successful")
+    except MySQLdb.Error as e:
+        print(f"The error '{e}' occurred")
+    return connection'''
+
+#ONLINE  HOST
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="sql12.freesqldatabase.com	", 
+            user="sql12707743",
+            password="EfzR55xMVG",
             database="sql12707743"
         )
         if connection.is_connected():
